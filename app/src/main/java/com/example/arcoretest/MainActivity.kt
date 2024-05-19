00ae6ef426f20a18a162e3b251fc68ff1ec7a5b4
kooo5252 <kooo5252@gmail.com>
2024-05-19 14:36:35 +0900

    ペンギンを表示できるとこまで進めた

--- app/src/main/java/com/example/arcoretest/MainActivity.kt
+++ app/src/main/java/com/example/arcoretest/MainActivity.kt
@@ -11,6 +11,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.arcoretest.ui.theme.ARcoreTestTheme
+import io.github.sceneview.Scene
+import io.github.sceneview.math.Position
+import io.github.sceneview.node.ModelNode
+import io.github.sceneview.rememberEngine
+import io.github.sceneview.rememberEnvironmentLoader
+import io.github.sceneview.rememberModelLoader
+import io.github.sceneview.rememberNodes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,5 +63,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ARView(){
-    Text(text = "Hello, ARCore!")
+    val engine = rememberEngine()
+    val modelLoader = rememberModelLoader(engine)
+    val environmentLoader = rememberEnvironmentLoader(engine)
+    val context = LocalContext.current
+    Scene(
+        modifier = Modifier.fillMaxSize(),
+        engine = engine,
+        modelLoader = modelLoader,
+        childNodes = rememberNodes {
+            add(ModelNode(modelLoader.createModelInstance(R.raw.penguin)).apply {
+                // Move the node 4 units in Camera front direction
+                position = Position(z = -4.0f)
+            })
+        },
+//        environment = environmentLoader.createHDREnvironment("environment.hdr")!!
+    )
+
 }
